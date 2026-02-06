51b539183323d7ba37b7ea5dc7028c7b5198ee89
ChaitanyaKrishnaGit <aschaitanyakrishna@gmail.com>
2026-02-06 13:36:24 +0530

    Exp2

---
+++ exp2.py
@@ -0,0 +1,8 @@
+n1=int(input("Enter Number1: "))
+n2=int(input("Enter Number2: "))
+n3=int(input("Enter Number3: "))
+total=n1+n2+n3
+average=total/3
+print("Total=", total)
+print("average=", average)
+
